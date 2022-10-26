f007908bd9ec1494858690474d30153add881cfd
Nail Gilaziev <nail.gilaziev@gmail.com>
2022-10-26 19:41:38 +0300

    init

---
+++ lib/labeled_checkbox.dart
@@ -0,0 +1,29 @@
+library common_widgets;
+
+import 'package:flutter/material.dart';
+
+class LabeledCheckbox extends StatelessWidget {
+  const LabeledCheckbox({
+    Key key,
+    this.value,
+    this.title,
+    this.onChanged,
+  }) : super(key: key);
+
+  final bool value;
+  final String title;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Checkbox(
+          value: value,
+          onChanged: onChanged,
+        ),
+        Expanded(child: Text(title)),
+      ],
+    );
+  }
+}
